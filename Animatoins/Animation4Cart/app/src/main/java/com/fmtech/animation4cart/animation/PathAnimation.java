ddec991fc3cdfc271bb1b3a5cd0ae23cda0e8953
Drew.Chiang <jiangchuna@126.com>
2016-06-02 21:45:28 +0800

    Accomplish PathAnimation.

--- Animatoins/Animation4Cart/app/src/main/java/com/fmtech/animation4cart/animation/PathAnimation.java
+++ Animatoins/Animation4Cart/app/src/main/java/com/fmtech/animation4cart/animation/PathAnimation.java
@@ -1,5 +1,24 @@
 package com.fmtech.animation4cart.animation;
 
-public class PathAnimation extends Animation{
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
+public class PathAnimation extends Animation {
+
+    private PathMeasure mPathMeasure;
+    private float[] pos = new float[2];
+
+    public PathAnimation(Path path) {
+        super();
+        mPathMeasure = new PathMeasure(path, false);
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation transformation) {
+        super.applyTransformation(interpolatedTime, transformation);
+        mPathMeasure.getPosTan(mPathMeasure.getLength() * interpolatedTime, pos, null);
+        transformation.getMatrix().setTranslate(pos[0], pos[1]);
+    }
 }
