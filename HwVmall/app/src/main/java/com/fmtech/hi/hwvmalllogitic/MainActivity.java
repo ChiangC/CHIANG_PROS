1f7c48f7b09809f6dba7594a24b19735f1a4c8d8
Drew.Chiang <jiangchuna@126.com>
2016-06-26 19:13:38 +0800

    Found some awsome thing in EMUI Theme.

--- HwVmall/app/src/main/java/com/fmtech/hi/hwvmalllogitic/MainActivity.java
+++ HwVmall/app/src/main/java/com/fmtech/hi/hwvmalllogitic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fmtech.hi.hwvmalllogitic;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int resId = getResources().getIdentifier("androidhwext:style/Theme.Emui", null, null);
+        if(resId > 0) {
+            setTheme(resId);
+            ActionBar actionBar = getActionBar();
+            actionBar.hide();
+        }
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         mLogisticList = (ListView)findViewById(R.id.lv_logistics_list);
@@ -39,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData(){
+        mLogisticInfos.add(0, new LogisticInfo("2016/06/22", "03:10:20", "快件离开【北京集散中心】，正发往【上海总集散中心】"));
+        mLogisticInfos.add(0, new LogisticInfo("2016/06/22", "12:25:00", "快件到达【上海总集散中心】"));
+        mLogisticInfos.add(0, new LogisticInfo("2016/06/23", "10:10:20", "快件离开【上海集散中心】，正发往【广州总集散中心】"));
+        mLogisticInfos.add(0, new LogisticInfo("2016/06/23", "16:25:00", "快件到达【广州总集散中心】"));
         mLogisticInfos.add(0, new LogisticInfo("2016/06/23", "14:40:20", "快件离开【广州总集散中心】，正发往【深圳总集散中心】"));
         mLogisticInfos.add(0, new LogisticInfo("2016/06/23", "16:25:00", "快件到达【深圳总集散中心】"));
         mLogisticInfos.add(0, new LogisticInfo("2016/06/23", "18:05:00", "快件离开【深圳总集散中心】，正发往【长沙集散中心】"));
@@ -47,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         mLogisticInfos.add(0, new LogisticInfo("2016/06/24", "14:34:00", "快件到达【永州】"));
         mLogisticInfos.add(0, new LogisticInfo("2016/06/24", "15:29:00", "快件离开【永州】，正发往【永州东安】"));
         mLogisticInfos.add(0, new LogisticInfo("2016/06/24", "16:46:00", "快件到达【永州东安】"));
+        mLogisticInfos.add(0, new LogisticInfo("2016/06/25", "09:10:00", "快件正在派送途中，请您准备签收(派件人：xxx,电话：18877668888)"));
     }
 
     private void updateViews(){
@@ -61,5 +75,35 @@ public class MainActivity extends AppCompatActivity {
             mLogisticList.setVisibility(View.VISIBLE);
         }
     }
-
+/*
+    @SuppressLint({"NewApi"})
+    protected void onCreate(Bundle paramBundle)
+    {
+        Logger.i("BaseActivity", "onCreate()");
+        this.e = this;
+        this.a = getResources().getIdentifier("androidhwext:style/Theme.Emui", null, null);
+        if (this.a == 0)
+        {
+            setTheme(2131230764);
+            this.b = getActionBar();
+            this.b.setLogo(new ColorDrawable(0));
+            ((TextView)findViewById(Resources.getSystem().getIdentifier("action_bar_title", "id", "android"))).setTextColor(this.e.getResources().getColor(2131296256));
+        }
+        for (;;)
+        {
+            this.b.setDisplayHomeAsUpEnabled(true);
+            this.b.show();
+            super.onCreate(paramBundle);
+            return;
+            if (Constants.IS_EMUI_3EX1_UP)
+            {
+                Logger.i("BaseActivity", "isEmui4");
+                setTheme(2131230771);
+            }
+            this.b = getActionBar();
+            if (!Constants.IS_EMUI3_UP) {
+                this.b.setBackgroundDrawable(this.e.getResources().getDrawable(2131296282));
+            }
+        }
+    }*/
 }
