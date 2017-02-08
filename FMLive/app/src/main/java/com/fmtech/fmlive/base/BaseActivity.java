62cb70e152522d842292d8de72a611fb2af58639
Drew.Chiang <drew_jiang@mtelnet.com>
2017-02-08 11:37:01 +0800

    BaseActivity.

--- FMLive/app/src/main/java/com/fmtech/fmlive/base/BaseActivity.java
+++ FMLive/app/src/main/java/com/fmtech/fmlive/base/BaseActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * ==================================================================
@@ -20,7 +22,7 @@ import android.view.View;
  */
 
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
     protected Context mContext;
     protected Handler mHanlder = new Handler();
 
@@ -32,13 +34,72 @@ public class BaseActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         mContext = this;
 
+        if(getLayoutResId() > 0){
+            setContentView(getLayoutResId());
+            initViews();
+            initData();
+            setupListeners();
+        }
+
     }
 
     protected void setBeforeLayout(){
 
     }
 
+    protected abstract int getLayoutResId();
+
+
+    protected abstract void initViews();
+
+
+    protected abstract void initData();
+
+
+    protected abstract void setupListeners();
+
+
     public <T extends View> T obtainView(int resId){
         return (T)findViewById(resId);
     }
+
+
+    public Toast showToast(int resId){
+        return showToast(getString(resId));
+    }
+
+    public Toast showToast(String message){
+        if(TextUtils.isEmpty(message)){
+            return null;
+        }
+
+        final Toast toast = Toast.makeText(BaseActivity.this, message, Toast.LENGTH_SHORT);
+        mHanlder.post(new Runnable() {
+            @Override
+            public void run() {
+                toast.show();
+            }
+        });
+        
+        return toast;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
