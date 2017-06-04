707dba5a37166dfd114a5e6f4be1ea324dace4d1
Drew.Chiang <jiangchuna@126.com>
2017-06-04 18:37:25 +0800

    FlowLayout.java

---
+++ AndroidUI/FlowLayout/app/src/main/java/com/fmtech/flowlayout/view/FlowLayout.java
@@ -0,0 +1,48 @@
+package com.fmtech.flowlayout.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * ==================================================================
+ * Copyright (C) 2017 FMTech All Rights Reserved.
+ *
+ * @author Drew.Chiang
+ * @version v1.0.0
+ * @email chiangchuna@gmail.com
+ * @create_date 2017/6/4 18:34
+ * @description ${todo}
+ * <p>
+ * ==================================================================
+ */
+
+
+public class FlowLayout extends ViewGroup {
+
+    public FlowLayout(Context context) {
+        super(context);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
