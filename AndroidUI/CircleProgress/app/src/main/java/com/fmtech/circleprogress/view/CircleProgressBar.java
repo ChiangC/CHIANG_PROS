20833975af853f605d68cae8f93c683f1cc51683
Drew.Chiang <jiangchuna@126.com>
2017-06-07 17:51:53 +0800

    draw circle progress.

---
+++ AndroidUI/CircleProgress/app/src/main/java/com/fmtech/circleprogress/view/CircleProgressBar.java
@@ -0,0 +1,111 @@
+package com.fmtech.circleprogress.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.fmtech.circleprogress.R;
+
+/**
+ * ==================================================================
+ * Copyright (C) 2017 MTel Limited All Rights Reserved.
+ *
+ * @author Drew.Chiang
+ * @version v1.0.0
+ *
+ * ==================================================================
+ */
+
+
+public class CircleProgressBar extends View {
+    private Paint mPaint;
+    private int mDefaultCircleColor = Color.parseColor("#d3d3d3");
+    private int mDefaultProgressColor = Color.parseColor("#04bce4");
+
+    private int mCircleColor;
+    private int mProgressColor;
+    private int mMaxProgress;
+    private float mCircleWidth;
+    private int mTextColor;
+    private float mTextSize;
+    private int mProgress = 90;
+    private float mRadius;
+    private float mHalfWidth;
+
+    public CircleProgressBar(Context context) {
+        this(context, null);
+    }
+
+    public CircleProgressBar(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleProgressBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleProgressBar);
+        mCircleColor = typedArray.getColor(R.styleable.CircleProgressBar_circleColor, mDefaultCircleColor);
+        mProgressColor = typedArray.getColor(R.styleable.CircleProgressBar_progressColor, mDefaultProgressColor);
+        mMaxProgress = typedArray.getInt(R.styleable.CircleProgressBar_maxProgress, 360);
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        mCircleWidth = typedArray.getDimension(R.styleable.CircleProgressBar_circleWidth, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, displayMetrics));
+        mTextColor = typedArray.getColor(R.styleable.CircleProgressBar_textColor, mDefaultProgressColor);
+        mTextSize = typedArray.getDimension(R.styleable.CircleProgressBar_textSize, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 20, displayMetrics));
+        typedArray.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        mHalfWidth = getWidth()/2;
+        mRadius = mHalfWidth - mCircleWidth/2;
+
+        mPaint = new Paint();
+
+        mPaint.setColor(mCircleColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(mCircleWidth);
+        canvas.drawCircle(mHalfWidth, mHalfWidth, mRadius, mPaint);
+
+        mPaint.reset();
+        mPaint.setColor(mTextColor);
+        mPaint.setTextSize(mTextSize);
+        mPaint.setAntiAlias(true);
+        String percent = (mProgress*100/mMaxProgress)+"%";
+        float textWidth = mPaint.measureText(percent);
+        Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
+        canvas.drawText(percent, (mHalfWidth - textWidth/2), mHalfWidth + (fontMetrics.bottom - fontMetrics.top)/2, mPaint);
+
+        drawProgress(canvas);
+
+    }
+
+    private void drawProgress(Canvas canvas){
+        mPaint.reset();
+        mPaint.setColor(mProgressColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(mCircleWidth);
+        RectF oval = new RectF(mCircleWidth/2, mCircleWidth/2, mHalfWidth*2- mCircleWidth/2, mHalfWidth*2 - mCircleWidth/2);
+        canvas.drawArc(oval, -90f, mProgress*360f/mMaxProgress, false, mPaint);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+    }
+
+
+}
