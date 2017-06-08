4f9c636c5966e11b17e91443ecb9c9b35bc12e4e
Drew.Chiang <jiangchuna@126.com>
2017-06-08 21:49:18 +0800

    LinearGradientTextView

---
+++ AndroidUI/LinearGradientTextView/app/src/main/java/com/fmtech/lineargradienttextview/widget/LinearGradientTextView.java
@@ -0,0 +1,35 @@
+package com.fmtech.lineargradienttextview.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * ==================================================================
+ * Copyright (C) 2017 FMTech All Rights Reserved.
+ *
+ * @author Drew.Chiang
+ * @version v1.0.0
+ * @email chiangchuna@gmail.com
+ * @create_date 2017/6/8 21:46
+ * @description ${todo}
+ * <p>
+ * ==================================================================
+ */
+
+
+public class LinearGradientTextView extends TextView {
+
+    public LinearGradientTextView(Context context) {
+        super(context);
+    }
+
+    public LinearGradientTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LinearGradientTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
